143573027e6659a247a079213d5b2d666251da8f
radeth <123radeth@gmail.com>
2019-05-15 00:45:36 +0200

    initial commit

---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from './Hero'
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.css']
+})
+export class HerosComponent implements OnInit {
+  hero: Hero ={
+    id:1,
+    name:'magneto'
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
